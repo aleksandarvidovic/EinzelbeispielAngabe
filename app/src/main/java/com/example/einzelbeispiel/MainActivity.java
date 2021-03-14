ed748664a566485e21582ef2d2812497f0ddf6a1
Aleksandar Vidovic <alvidovic@edu.aau.at>
2021-03-14 01:12:08 +0100

    Benutzerschnittstelle erstellt

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.einzelbeispiel;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Network Test");
+    }
+
+    public void send (View view){
+
+        //
     }
 }
