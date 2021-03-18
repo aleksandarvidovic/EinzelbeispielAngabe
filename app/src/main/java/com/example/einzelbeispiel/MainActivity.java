c5e36a1f7c2f760889b096147ce9686cfb6482b5
Aleksandar Vidovic <alvidovic@edu.aau.at>
2021-03-18 02:18:45 +0100

    Verbunden zum Server und ASCII berechnet(Projekt fertig)

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -7,10 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity  {
+
+    private EditText editText;
+    private TextView textView;
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,31 +28,105 @@ public class MainActivity extends AppCompatActivity  {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Network Test");
+
+        editText = (EditText)findViewById(R.id.editTextNumber);
+        textView = (TextView)findViewById(R.id.textView3);
+        text = "";
     }
 
-    public void send (View view){
+    public void send (View view) {
 
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
 
-                EditText editText = (EditText)findViewById(R.id.editTextNumber);
-                String et = editText.getText().toString();
+                Socket socket = null;
+
                 try {
-                    Client client = new Client(et);
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+                    socket = new Socket("se2-isys.aau.at", 53212);
+
+                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+                    printWriter.println(editText.getText().toString());
+                    printWriter.flush();
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    text = bufferedReader.readLine();
+
+                } catch (Exception e) {
+
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                 }
 
                 try {
-                    Server server = new Server();
-                    TextView textView = (TextView)findViewById(R.id.textView3);
-                    textView.setText(server.getMessage());
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+                    socket.close();
+
+                } catch (Exception e) {
+
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
+
         thread.start();
+
+        try {
+
+            thread.join();
+
+        } catch (Exception e) {
+
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                textView.setText(text);
+            }
+        });
+    }
+
+    public void calculate(View view){
+
+        if(!(editText.getText().toString()).equals("11741375"))
+            Toast.makeText(MainActivity.this, "Das ist falsche Matrikelnummer!", Toast.LENGTH_LONG).show();
+        else{
+
+            String num = editText.getText().toString();
+
+            char[] array = new char[num.length()];
+
+            for(int i = 0; i < num.length(); i++)
+                array[i] = num.charAt(i);
+
+            for(int i = 1; i < array.length; i += 2){
+
+                if(array[i] == '1')
+                    array[i] = 'a';
+                else if(array[i] == '2')
+                    array[i] = 'b';
+                else if(array[i] == '3')
+                    array[i] = 'c';
+                else if(array[i] == '4')
+                    array[i] = 'd';
+                else if(array[i] == '5')
+                    array[i] = 'e';
+                else if(array[i] == '6')
+                    array[i] = 'f';
+                else if(array[i] == '7')
+                    array[i] = 'g';
+                else if(array[i] == '8')
+                    array[i] = 'h';
+                else if(array[i] == '9')
+                    array[i] = 'i';
+            }
+
+            String asciiNum = new String(array);
+
+            textView.setText(asciiNum);
+        }
     }
 }
